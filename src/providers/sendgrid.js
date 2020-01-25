6d55d1704e9376ac5ab7a4a1717cec36d17ff459
Carsten Jacobsen <car.jacobsen@gmail.com>
2020-01-25 18:47:30 +0100

    fix(sendgrid): fix issue with new API version

--- src/providers/sendgrid.js
+++ src/providers/sendgrid.js
@@ -1,11 +1,27 @@
-const Sendgrid = require('@sendgrid/mail')
-const Log = require('../log')
+const Sendgrid = require("@sendgrid/mail");
+const Log = require("../log");
 
 module.exports = function(options = {}) {
-  const { key } = options
-  if (!key) return Log.error('Invalid provider setup')
+  const { key } = options;
+  if (!key) return Log.error("Invalid provider setup");
 
-  Sendgrid.setApiKey(key)
+  Sendgrid.setApiKey(key);
 
-  return Sendgrid
-}
+  return {
+    send(data) {
+      if (typeof data.from === "string") data.from = { email: data.from };
+      if (typeof data.to === "string") data.to = { email: data.to };
+
+      data.content = [
+        {
+          type: "text/html",
+          value: data.html
+        }
+      ];
+
+      delete data.html;
+
+      return Sendgrid.send(data);
+    }
+  };
+};
